0630b942815de34a43f62f2cf5032ef7b8c57b3b
princess butterflyy <ONYYX24@GMAIL.COM>
2024-02-08 23:53:12 -0700

    update html for current temp

--- scr/index.js
+++ scr/index.js
@@ -14,6 +14,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
